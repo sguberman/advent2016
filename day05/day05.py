556d878d059da916aea0849f5d631dfc7db62ac1
Seth Guberman <sguberman@gmail.com>
2016-12-09 19:09:04 -0500

    complete day05 part 2 from iOS

--- day05/day05.py
+++ day05/day05.py
@@ -10,22 +10,29 @@ def crack_password(door_id, length=8, start=0):
         hash_hex = md5(door_id + idx).hexdigest()
         if hash_hex.startswith('00000'):
             password += hash_hex[5]
+            print('{:<20}\t{} of {}'.format(password, len(password), length))
         int_idx += 1
     return password
 
 
 def crack_password2(door_id, length=8, start=0):
-    password = [' '] * length
+    password = ['#'] * length
     int_idx = start
     door_id = bytes(door_id, encoding='utf-8')
-    while ' ' in password:
+    while '#' in password:
         idx = bytes(str(int_idx), encoding='utf-8')
         hash_hex = md5(door_id + idx).hexdigest()
         if hash_hex.startswith('00000'):
             pos = hash_hex[5]
+            try:
+                pos = int(pos)
+            except ValueError:
+                int_idx += 1
+                continue
             value = hash_hex[6]
-            if pos in range(length) and password[pos] == ' ':
+            if pos in range(length) and password[pos] == '#':
                 password[pos] = value
+                print(''.join(password))
         int_idx += 1
     return ''.join(password)
 
--- day05/test_day05.py
+++ day05/test_day05.py
@@ -1,10 +1,17 @@
 from day05 import crack_password, crack_password2
+import unittest
 
 
-def test_crack_password():
-    assert crack_password('abc') == '18f47a30'
-    assert crack_password('uqwqemis') == '1a3099aa'
+class TestCrackPassword(unittest.TestCase):
+    def test_crack_password(self):
+        self.assertEqual(crack_password('abc'), '18f47a30')
+        self.assertEqual(crack_password('uqwqemis'), '1a3099aa')
+
+    def test_crack_password2(self):
+        self.assertEqual(crack_password2('abc'), '05ace8e3')
+        self.assertEqual(crack_password2('uqwqemis'), '694190cd')
 
 
-def test_crack_password2():
-    assert crack_password2('abc') == '05ace8e3'
+if __name__ == '__main__':
+    unittest.main()
+
