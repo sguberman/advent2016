1a1f5d725a0b78f1aa438839e54132311d9b5fbd
Seth Guberman <sguberman@gmail.com>
2017-01-27 13:47:07 -0500

    fix indentation on day11.py

--- day11/day11.py
+++ day11/day11.py
@@ -118,7 +118,7 @@ class Point(namedtuple('Point', 'elevator microchips generators')):
         combos = combinations(idxs, 2)
         for combo, dir in product(combos, (-1, 1)):  # move two pieces up or down
             states.append([elevator + dir] + [x + dir if i in combo else x
-                                            for i, x in enumerate(items)])
+                                              for i, x in enumerate(items)])
         for idx, dir in product(idxs, (1, -1)):  # move one piece up or down
             states.append([elevator + dir] + [x + dir if i == idx else x
                                               for i, x in enumerate(items)])
