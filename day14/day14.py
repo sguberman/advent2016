1c4c755cd570b7acc03366f4c888490b7c086686
Seth Guberman <sguberman@gmail.com>
2017-02-02 13:37:03 -0500

    complete part2

--- day14/day14.py
+++ day14/day14.py
@@ -1,8 +1,11 @@
 from hashlib import md5
 
 
-def hash_hex(salt, idx):
-    return md5(bytes(salt + str(idx), encoding='utf-8')).hexdigest()
+def hash_hex(salt, idx, stretch=0):
+    h = md5(bytes(salt + str(idx), encoding='utf-8')).hexdigest()
+    for _ in range(stretch):
+        h = md5(bytes(h, encoding='utf-8')).hexdigest()
+    return h
 
 
 def first_triple(hexdigest):
@@ -16,13 +19,13 @@ def has_consecutive_digit(hexdigest, digit, times=5):
     return times * digit in hexdigest
 
 
-def is_key(salt, idx, hashes=None, window=1000):
+def is_key(salt, idx, hashes=None, window=1000, stretching=0):
     if hashes is None:
         hashes = []
     try:
         key = hashes[idx]
     except IndexError:
-        key = hash_hex(salt, idx)
+        key = hash_hex(salt, idx, stretching)
         hashes.append(key)
     digit = first_triple(key)
     if not digit:
@@ -31,20 +34,20 @@ def is_key(salt, idx, hashes=None, window=1000):
         try:
             hexdigest = hashes[i]
         except IndexError:
-            hexdigest = hash_hex(salt, i)
+            hexdigest = hash_hex(salt, i, stretching)
             hashes.append(hexdigest)
         if has_consecutive_digit(hexdigest, digit):
             return key, hashes
     return False, hashes
 
 
-def generate_n_keys(salt, n=64, start_idx=0, window=1000):
+def generate_n_keys(salt, n=64, start_idx=0, window=1000, stretching=0):
     up_to = n
     n = 0
     idx = start_idx
     known_hashes = None
     while n < up_to:
-        key, known_hashes = is_key(salt, idx, known_hashes, window)
+        key, known_hashes = is_key(salt, idx, known_hashes, window, stretching)
         if key:
             n += 1
             yield key, idx
@@ -52,6 +55,7 @@ def generate_n_keys(salt, n=64, start_idx=0, window=1000):
 
 
 if __name__ == '__main__':
-    keys = list(generate_n_keys('ahsbgdzn', n=64))
+    #keys = list(generate_n_keys('ahsbgdzn', n=64))
+    keys = list(generate_n_keys('ahsbgdzn', n=64, stretching=2016))
     key, idx = keys[-1]
     print(idx, key)
--- day14/test_day14.py
+++ day14/test_day14.py
@@ -39,8 +39,26 @@ def test_is_key(salt, idx):
 
 
 def test_generate_n_keys():
-    keys = [key for key in  generate_n_keys('abc', n=64)]
+    keys = [key for key in generate_n_keys('abc', n=64)]
     assert len(keys) == 64
     key, idx = keys[-1]
     print(idx, key)
     assert idx == 22728
+
+
+@pytest.mark.parametrize('salt,idx,stretch,expected', [
+    ('abc', 0, 0, '577571be4de9dcce85a041ba0410f29f'),
+    ('abc', 0, 1, 'eec80a0c92dc8a0777c619d9bb51e910'),
+    ('abc', 0, 2, '16062ce768787384c81fe17a7a60c7e3'),
+    ('abc', 0, 2016, 'a107ff634856bb300138cac6568c0f24'),
+])
+def test_hash_hex_stretching(salt, idx, stretch, expected):
+    assert hash_hex(salt, idx, stretch) == expected
+
+
+def test_generate_n_keys_with_stretching():
+    keys = [key for key in generate_n_keys('abc', n=64, stretching=2016)]
+    assert len(keys) == 64
+    key, idx = keys[-1]
+    print(idx, key)
+    assert idx == 22551
